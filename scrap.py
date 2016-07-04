6b57f6c814fbfa242ac4c971897f0c0c515e1589
ravenusmc <mcuddy77@gmail.com>
2016-07-04 10:38:38 -0400

    finished species functions

--- scrap.py
+++ scrap.py
@@ -1,94 +1,100 @@
+#This program will use the http://swapi.co/api/ api which will allow the user to look at
+#star wars data. 
 
-#Process the results
-# print(response_dict.keys())
+import requests 
 
-#printing information about the API:
-#Here I show that the API has a count of 37 objects. 
-# print("Total Starships: ", response_dict['count'])
+def connect(view):
+  #Setting up the URL that I will be looking at.
+  url = ('http://swapi.co/api/' + str(view))
+  #Setting up a get request to pull the data from the URL
+  r = requests.get(url)
+  if r.status_code == 200:
+    print("status_code", r.status_code)
+    print("You have connected to the database!")
+  else:
+    print("Sorry it appears your connection failed!")
 
-#Here I am showing what the ship in element 1 is. 
-#print(response_dict['results'][1])
+  #Storing the API response in a variable 
+  response_dict = r.json()
+  print("There are currently", response_dict['count'], "items to view")
 
-#Exploring information about the repositories.
-repo_dicts = response_dict['results']
-# print("repositories returned:", len(repo_dicts))
+  print("Here are the " + view + " that you can see:")
 
-#Examine the first repo
-repo_dict = repo_dicts[1]
-print("Keys:", len(repo_dict))
-#I will be able to see what data is available about each starship now. 
-for key in sorted(repo_dict.keys()):
-  print(key)
+  repo_dicts = response_dict['results']
 
-# print("\nSelected information about the Second starship:")
-# print("Name:", repo_dict['name'])
-# print("Crew: ", repo_dict['crew'])
-# print("max_atmosphering_speed:", repo_dict['max_atmosphering_speed'])
+  num = 0
+  while num < response_dict['count']:
+    if view == 'films':
+      repo_dict = repo_dicts[num]['title']
+      print(str(num) + " " + repo_dict)
+    elif view == 'starships':
+      repo_dict = repo_dicts[num]['name']
+      print(str(num) + " " + repo_dict)
+    num += 1 
 
-###Collecting information about more than one repo
-print("\nSelected information about each repo:")
+  choice = int(input("Which one do you want to look at: "))
+  print("Here is the information on the " + view + " you wanted to see: ")
+  # print(repo_dicts[choice])
+  items = []
+  item = {
+    "title": response_dict['results'][choice]['title'],
+    "episode_id": response_dict['results'][choice]['episode_id'],
+    "opening_crawl": response_dict['results'][choice]['opening_crawl'],
+    "director": response_dict['results'][choice]['director'],
+    "producer": response_dict['results'][choice]['producer'],
+    "release_date": response_dict['results'][choice]['release_date'],
+    "characters": response_dict['results'][choice]['characters'],
+  }
 
-#Creating two empty list to hold the names and crew data.
-# names, crew = [], []
-#I loop through the api and then print out each name and crew of each ship. 
-# for repo_dict in repo_dicts:
-  # print("\nName:", repo_dict['name'])
-  # print("Crew: ", repo_dict['crew'])
-  #Appending the data to the names and crew list 
-  # names.append(repo_dict['name'])
-  # crew.append(int(repo_dict['crew']))
+  items.append(item)
 
-#Making the visualization 
-# my_style = LS('#333366', base_style=LCS)
-# chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
-# chart.title = "Star Wars Ships and crew"
-# chart.x_labels = names
-
-# chart.add('', crew)
-# chart.render_to_file('starwars.svg')
+  for item in items:
+    print("Title:", item['title'])
+    print("episode:", response_dict['results'][choice]['episode_id'])
+    print("Director:", item['director'])
+    print("Producer:", item['producer'])
+    print("release_date", item['release_date'])
+    print("opening_crawl:", item['opening_crawl'])
 
 
+########## This Code is GOOD!!!
+  # num = 0
+  # while num < response_dict['count']:
+  #   if view == 'films':
+  #     repo_dict = repo_dicts[num]['title']
+  #     print(str(num) + " " + repo_dict)
+  #   elif view == 'starships': 
+  #     repo_dict = repo_dicts[num]['name']
+  #     print(str(num) + " " + repo_dict)
+  #   num += 1 
 
-#############
+  # choice = int(input("Which one do you want to look at: "))
+  # print("Here is the information on the " + view + "You wanted to see: ")
+  # print(repo_dicts[choice])
+###################
 
-Star Wars API 
+def began():
+  print("To start, Here are the categories you can look at:")
+  print("people")
+  print("planets")
+  print("films")
+  print("species")
+  print("vehicles")
+  print("starships")
+  view = input("What do you want to look at? ")
+  connect(view)
 
-0 Sentinel-class landing craft         1
-1 Death Star                           2
-2 Millennium Falcon                    3
-3 Y-wing                               4
-4 X-wing                               5
-5 TIE Advanced x1                      6
-6 Executor                             7
-7 Slave 11                             8
-8 Imperial shuttle                     9
-9 EF76 Nebulon-B escort frigate        10
 
-10 status_code, page 2 200             11
-11 status_code, page 2 200             12
-12 status_code, page 2 200             13
-13 status_code, page 2 200             14
-14 status_code, page 2 200             15
-15 status_code, page 2 200             16
-16 status_code, page 2 200             17
-17 status_code, page 2 200             18
-18 status_code, page 2 200             19
+#This function starts the program and basically asks the user if
+#they want to use it. 
+def main():
+  print('*******************')
+  print('---STAR WARS API--- ')
+  print('*******************')
+  choice = input("Would you like to use it?(y/n) ")
+  if choice.lower() == "y":
+    began()
+  else:
+    print("Hope you use it soon!")
 
-19 status_code, page 3 200
-20 status_code, page 3 200
-21 status_code, page 3 200
-22 status_code, page 3 200
-23 status_code, page 3 200
-24 status_code, page 3 200
-25 status_code, page 3 200
-26 status_code, page 3 200
-27 status_code, page 3 200
-28 status_code, page 3 200
-
-29 status_code, page 4 200
-30 status_code, page 4 200
-31 status_code, page 4 200
-32 status_code, page 4 200
-33 status_code, page 4 200
-34 status_code, page 4 200
-35 status_code, page 4 200
+main()
